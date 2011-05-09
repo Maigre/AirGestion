2f56596701d653926f2cd0a140474e2964dda962
Thomas BOHL <thomas.bohl@gmail.com>
2011-05-09 10:47:00 +0200

    MGR creating git-hub repo

---
+++ application/models/adherent.php
@@ -0,0 +1,179 @@
+<?php
+
+class Adherent extends DataMapper {
+	
+	var $has_one=array("csps", "situationfamiliales", "statutadherents");
+	var $has_many=array("familles");
+	
+	var $description = array(
+		'nom' => 	array(
+				'label' 	=> array('Nom',false),
+				'rules' 	=> array('required', 'xss_clean'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('text', 15)
+		),
+		'prenom' => 	array(
+				'label' 	=> array('Prénom',false),
+				'rules' 	=> array('required', 'xss_clean'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('text', 15)
+		)
+		'sexe' => 	array(
+				'label' 	=> array('Sexe'),
+				'rules' 	=> array('xss_clean'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('radio')
+		)
+		'datenaissance' => array(
+				'label' 	=> array('Date de naissance'),
+				'rules' 	=> array('required', 'xss_clean', 'valid_date'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('date')
+		)
+		'fichesanitaire' => array(
+				'label' 	=> array('Fiche Sanitaire'),
+				'rules' 	=> array('xss_clean'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('text', 15)
+		)
+		'email' => 	array(
+				'label' 	=> array('Email'),
+				'rules' 	=> array('xss_clean', 'valid_email'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('text', 30)
+		)
+		'telportable' => array(
+				'label' 	=> array('Tél. Portable'),
+				'rules' 	=> array('xss_clean', 'numeric', 'exact_length[10]'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('text', 10)
+		)
+		'teldomicile' => array(
+				'label' 	=> array('Tel. Domicile'),
+				'rules' 	=> array('xss_clean', 'numeric', 'exact_length[10]'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('text', 10)
+		)
+		'telprofessionel' => array(
+				'label' 	=> array('Tel. Professionel'),
+				'rules' 	=> array('xss_clean', 'numeric', 'exact_length[10]'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('text', 10)
+		)
+		'sansviandesansporc' => array(
+				'label' 	=> array('Sans Viande Sans Porc'),
+				'rules' 	=> array('xss_clean'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('check')
+		)
+		'autorisationsortie' => array(
+				'label' 	=> array('Autorisation de sortie'),
+				'rules' 	=> array('xss_clean'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('check')
+		)
+		'allocataire' => array(
+				'label' 	=> array('Allocataire'),
+				'rules' 	=> array('xss_clean'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('check')
+		)
+		'employeur' => 	array(
+				'label' 	=> array('Employeur'),
+				'rules' 	=> array('xss_clean'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('text', 15)
+		)
+		'noallocataire' => array(
+				'label' 	=> array('N° d\'Allocataire'),
+				'rules' 	=> array('xss_clean'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('text', 10)
+		)
+		'nosecu' => 	array(
+				'label' 	=> array('N° Sécu'),
+				'rules' 	=> array('xss_clean', 'exact_length[15]'),
+				'defval'	=> '',
+				'type'		=> 'normal',
+				'format'	=> array(),
+				'formfield' 	=> array('text', 15)
+		)
+		'csp' => 	array(
+				'label' 	=> array('CSP'),
+				'rules' 	=> array('xss_clean'),
+				'defval'	=> '',
+				'type'		=> 'link',
+				'format'	=> array(),
+				'formfield' 	=> array('listbox')
+		)
+		'situationfamiliale' => array(
+				'label' 	=> array('Situation familiale'),
+				'rules' 	=> array('xss_clean'),
+				'defval'	=> '',
+				'type'		=> 'link',
+				'format'	=> array(),
+				'formfield' 	=> array('listbox')
+		)
+		'statutadherent' => 	array(
+				'label' 	=> array('Statut de l\'adhérent'),
+				'rules' 	=> array('xss_clean'),
+				'defval'	=> '',
+				'type'		=> 'link',
+				'format'	=> array(),
+				'formfield' 	=> array('listbox')
+		)
+	);
+	
+	//Generation des templates
+		$tpl1 = '<table>';
+		foreach $description as $nom_champ=>$properties
+		{
+			$tpl1 .= '<tr><td>{'.$nom_champ.'}</td></tr>';
+		}
+		$tpl1 .= '</table>';
+	
+		'tpl2' => '<table>
+				<tr><td>{nom}</td></tr>
+			     </table>'
+		);
+	var $templates = array($tpl1,$tpl2);
+	
+	var $modes = array(
+		'display' => 'display',
+		'edit' => array('nom' => 'form')	
+		);
+
+	function Adherent()
+	{
+		parent::DataMapper();
+	}
+
+}
