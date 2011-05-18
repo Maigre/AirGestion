92f2f567bb9edc35ad071a4c7467b57947d0e25f
tamias <imbert.matthias@gmail.com>
2011-05-18 15:43:35 +0200

    TMS new panel into init

--- interface/apps/viewport.js
+++ interface/apps/viewport.js
@@ -3,8 +3,23 @@ Ext.ns('MainApp');
 
 function MenuItem() {
 		this.panel = null;
-		this.get = function(){return this.panel;}
+		this.get = function(){return this.panel;};
+		//this.load = function(){
+				
 	}
+	
+/*function windowcreate(){
+		var win = new Ext.Window({  
+					title: 'User form',
+					iconCls: 'user',  
+					bodyStyle: 'padding:10px;background-color:#fff;',  
+					width:400,
+					height:400, 
+					//items:[formcreate()],
+					layout: 'fit' 
+				});
+		return win;
+	}*/
 
 MainApp.ViewPort = {
 	
@@ -12,47 +27,19 @@ MainApp.ViewPort = {
 
 	Menu_Accueil : new MenuItem(),
 
-	Menu_Adherents : new Ext.Panel({
-			id: 'Menu_Adherents-panel',
-			title: 'Adherents',
-			iconCls: 'user',
-			html: 'Menu Adherents...'
-		}),
+	Menu_Adherents : new MenuItem(),
 	
-	Menu_Activites : new Ext.Panel({
-			id: 'Menu_Activites-panel',
-			title: 'Activites',
-			iconCls: 'palette',
-			html: 'Menu Activites...'
-		}),
+	Menu_Activites : new MenuItem(),
 	
-	Menu_Clsh : new Ext.Panel({
-			id: 'Menu_Clsh-panel',
-			title: 'Centre de Loisirs',
-			iconCls: 'slide',
-			html: 'Menu CLSH...'
-		}),
+	Menu_Clsh : new MenuItem(),
 	
-	Menu_Perisco : new Ext.Panel({
-			id: 'Menu_Perisco-panel',
-			title: 'Periscolaire',
-			iconCls: 'ruler',
-			html: 'Menu Periscolaire...'
-		}),
+	Menu_Perisco : new MenuItem(),
 	
-	Menu_Paiements : new Ext.Panel({
-			id: 'Menu_Paiements-panel',
-			title: 'Paiements',
-			iconCls: 'money',
-			html: 'Menu Paiements...'
-		}),
+	Menu_Paiements : new MenuItem(),
 
-	Menu_Config : new Ext.Panel({
-			id: 'Menu_Config-panel',
-			title: 'Configuration',
-			iconCls: 'cog',
-			html: 'Menu Configuration...'
-		}),
+	Menu_Config : new MenuItem(),
+	
+	Window_User : new MenuItem(),
 	
 	init : function() {
 	
@@ -62,6 +49,52 @@ MainApp.ViewPort = {
 				iconCls: 'house',
 				html: 'Menu Accueil...'
 			});
+		this.Menu_Adherents.panel = new Ext.Panel({
+				id: 'Menu_Adherents-panel',
+				title: 'Adherents',
+				iconCls: 'user',
+				html: 'Menu Adherents...',
+				url: 'interface/menu/adherent'
+			});
+		this.Menu_Activites.panel = new Ext.Panel({
+				id: 'Menu_Activites-panel',
+				title: 'Activites',
+				iconCls: 'palette',
+				html: 'Menu Activites...'
+			});
+		this.Menu_Clsh.panel = new Ext.Panel({
+				id: 'Menu_Clsh-panel',
+				title: 'Centre de Loisirs',
+				iconCls: 'slide',
+				html: 'Menu CLSH...'
+			});
+		this.Menu_Perisco.panel = new Ext.Panel({
+				id: 'Menu_Perisco-panel',
+				title: 'Periscolaire',
+				iconCls: 'ruler',
+				html: 'Menu Periscolaire...'
+			});
+		this.Menu_Paiements.panel = new Ext.Panel({
+				id: 'Menu_Paiements-panel',
+				title: 'Paiements',
+				iconCls: 'money',
+				html: 'Menu Paiements...'
+			});
+		this.Menu_Config.panel = new Ext.Panel({
+				id: 'Menu_Config-panel',
+				title: 'Configuration',
+				iconCls: 'cog',
+				html: 'Menu Configuration...'
+			});
+		this.Window_User.panel = new Ext.Window({  
+					title: 'User form',
+					bodyStyle: 'padding:10px;background-color:#fff;',  
+					width:400,
+					height:400,
+					constrain: true
+			});
+		//this.Window_User.panel.show();
+		
 	
 		this.AppPort = new Ext.Viewport({
 				id: 'MainApp-viewport',
@@ -71,7 +104,7 @@ MainApp.ViewPort = {
 					html: '<div id="title-img" style="padding:7px 50px"><img src="images/airgestion-tr.png" alt="AirGestion!" height="45px" width="auto" /></div>',
 					bodyStyle: "background-image:url(images/bck3.jpg)",
 					heiht: 200,
-					border: true
+					border: true,
 				}, {
 					region: 'west',
 					width:200,
@@ -86,17 +119,24 @@ MainApp.ViewPort = {
 							animate: true,
 							activeOnTop: true
 						},
-					items:[this.Menu_Accueil.get(),this.Menu_Adherents,this.Menu_Activites,this.Menu_Clsh,this.Menu_Perisco,this.Menu_Paiements,this.Menu_Config]
+					items:[this.Menu_Accueil.get(),this.Menu_Adherents.get(),this.Menu_Activites.get(),this.Menu_Clsh.get(),this.Menu_Perisco.get(),this.Menu_Paiements.get(),this.Menu_Config.get()]
 				}, {
-					region: 'center'
-					//,bodyStyle: "background-image:url(images/bck3.jpg)"
+					region: 'center',
+					//layout: 'auto',
+					id: 'viewport_center_region'
 				}, {
 					region: 'south',
 					html: '',
 					bodyStyle: "background-image:url(images/bck3.jpg)",
 					height: 30
 				}]
-			});  	
+			});
+		
+		
+		this.AppPort.layout.regions.center.add(this.Window_User.get()); // using add()
+        this.Window_User.get().show();
+        
+        //this.AppPort.endUpdate();  	
 	},
 	
 	reload: function() {
