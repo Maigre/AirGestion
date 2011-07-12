f3c8b924b764a09e7de95a5ec86ae3331b839a2a
Tamias <tamias@indiAir.(none)>
2011-07-12 10:51:02 +0200

    TMS add files

---
+++ application/views/MJC/double_famille.php
@@ -0,0 +1,79 @@
+<script type="text/javascript">
+
+MainApp.Content = {
+
+
+	make : function () {
+
+		// ----------------
+		//	Create datasource
+		// ----------------
+
+	
+		var mydata = [
+				<?php $count=0;
+				foreach($familles as $famille){?>
+					<?php if ($count>0) {echo ',';}?>
+					<?php $count=$count+1 ?>
+					['<?=$famille['nom']?>','<?=$famille['prenom']?>',<?=$famille['id']?>,<?=$famille['idFamille']?>]
+				<?php } ?>
+		];
+		
+		var ds = new Ext.data.ArrayStore({
+			// store configs
+			//autoDestroy: true,
+			//storeId: 'myStore',
+			// reader configs
+			//idIndex: 0,
+			fields: [
+				{name: 'nom',  type: 'string'},
+				{name: 'prenom',  type: 'string'},
+				{name: 'id',    type: 'int'},
+				{name: 'idFamille',    type: 'int'}
+			]
+		});
+		ds.loadData(mydata);
+	 
+		// ----------------
+		//	Create grid
+		// ----------------	
+
+		this.searchgrid = new Ext.grid.GridPanel(
+		{
+			id: 'searchgrid',
+			store: ds, // use the datasource
+			columns: [
+				{dataIndex: 'nom', flex:1, sortable: true},
+				{dataIndex: 'prenom',  sortable: true},
+				{dataIndex: 'id',  hidden: true},
+				{dataIndex: 'idFamille',  hidden: true}
+			],
+			hideHeaders: true,
+			autoScroll: true,
+			listeners: {
+				itemclick: function(g,i){ 
+						askAndDo(MainApp.ViewPort.AppPort.layout.regions.center.id,'interface/c_famille/display/'+i.data.idFamille+'/'+i.data.id);			    	
+						Ext.getCmp('doublefamilleWindow').close();
+				}
+			},
+			stripeRows:true,
+			autoHeight:true 
+		});	
+
+	
+		doublefamilleWindow = new Ext.Window({
+					id: 'doublefamilleWindow',
+					title: 'Sélection famille par adhérent',
+					width: 240,
+					layout: 'fit',
+					modal: true,
+					closable: true,
+					resizable: true,
+					items: [this.searchgrid],
+					url: BASE_URL+'interface/c_adherent/save/1'
+				});
+		Ext.getCmp('doublefamilleWindow').show();	
+	}	
+}
+MainApp.Content.make();
+</script>
